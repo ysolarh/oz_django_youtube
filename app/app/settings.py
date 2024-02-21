f4a7c8e3e176a16be1b06ee052e59f116596a6db
ysolarh <ysolarh@gmail.com>
2024-02-22 00:03:28 +0900

    Chore: update .dockerignore, settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-l+xn)jyey1%*@ti9vfd+t7gr)2l+2@rw1b_&91o@p=cokv_#%f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['0.0.0.0']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
